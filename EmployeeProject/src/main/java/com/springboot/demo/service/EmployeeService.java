f07bb94cdea9992de3005fa45703099136604efa
notepower2k1 <83808551+notepower2k1@users.noreply.github.com>
2022-04-03 22:11:53 +0700

    Add files via upload

---
+++ EmployeeProject/src/main/java/com/springboot/demo/service/EmployeeService.java
@@ -0,0 +1,9 @@
+package com.springboot.demo.service;
+
+import java.util.List;
+
+import com.springboot.demo.model.Employee;
+
+public interface EmployeeService {
+	 List<Employee> getAllEmployees();
+}
